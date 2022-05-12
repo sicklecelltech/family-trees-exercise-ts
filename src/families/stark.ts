4c5d8682745503fbcba27c86eacc240450d65746
Tiffany Wilmot-Simpson <twilmotsimpson@gmail.com>
2022-05-12 12:02:41 +0100

    task 1, 2 and part of 4 completed

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -2,5 +2,23 @@ import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
 
-    return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
+    const eddardChildren :Person[] =[
+        {name:"Rickon", children: [] },
+        {name:"Bran", children: [] },
+        {name:"Ayra", children: [] },
+        {name:"Sansa", children: [] },
+        {name:"Robb", children: [] },
+        {name:"Jon", children: [] }
+    ]
+
+    const rickardsChildren :Person [] = [
+        {name:"Benjen", children: [] },
+        {name:"Lyanna", children: [] },
+        {name:"Eddard", children: eddardChildren },
+        {name:"Brandon", children: []}
+    ]
+    
+    const topPerson :Person = { name: "Rickard",  children: rickardsChildren }
+
+    return topPerson;
 }
--- src/treeLogging.ts
+++ src/treeLogging.ts
@@ -3,7 +3,28 @@ import { Person } from "./personTypes";
 
 export function logAllPeopleInTree(topPerson: Person): void {
     console.log("TODO!  First person is " + topPerson.name);
+    //array of all names
+    const stackOfPeople:Person[] =[];
+    //push the name of the top person
+    stackOfPeople.push(topPerson)
+
+    //while array.length is not 0
+    while (stackOfPeople.length!==0){
+    // check if the children element is empty
+        if (stackOfPeople[0].children.length > 0){
+            //loop through array and record all names
+            for (let child of stackOfPeople[0].children ){
+                stackOfPeople.push(child)
+            }          
+        }
+    //consolelog first elemet of the array
+    //remove the first element 
+        console.log(stackOfPeople[0].name)
+        stackOfPeople.splice(0,1)
+    }
+   
 }
+
 
 export function logAllPeopleInTreeWithQueue(topPerson: Person): void {
     console.log("TODO!  First person is " + topPerson.name);
